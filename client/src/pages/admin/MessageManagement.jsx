76d34cc621b7a8611d6c554ff429cbb87d963c1a
Vatsal bhalla <118548880+vatsalbhalla04@users.noreply.github.com>
2025-04-13 22:43:41 +0530

    Meesage Comp. Admin

--- client/src/pages/admin/MessageManagement.jsx
+++ client/src/pages/admin/MessageManagement.jsx
@@ -1,13 +1,12 @@
-import { useFetchData } from "6pp";
-import { Avatar, Box, Stack } from "@mui/material";
-import moment from "moment";
-import React, { useEffect, useState } from "react";
+/* eslint-disable react/jsx-key */
+import { useEffect, useState } from "react";
 import AdminLayout from "../../components/layout/AdminLayout";
-import RenderAttachment from "../../components/shared/RenderAttachment";
 import Table from "../../components/shared/Table";
-import { server } from "../../constants/config";
-import { useErrors } from "../../hooks/hook";
+import { dashboardData } from "../../constants/sampleData";
 import { fileFormat, transformImage } from "../../lib/features";
+import moment from "moment";
+import { Avatar, Box, Stack } from "@mui/material";
+import RenderAttachment from "../../components/shared/RenderAttachment";
 
 const columns = [
   {
@@ -21,6 +20,7 @@ const columns = [
     headerName: "Attachments",
     headerClassName: "table-header",
     width: 200,
+    
     renderCell: (params) => {
       const { attachments } = params.row;
 
@@ -45,6 +45,7 @@ const columns = [
             );
           })
         : "No Attachments";
+
     },
   },
 
@@ -52,7 +53,7 @@ const columns = [
     field: "content",
     headerName: "Content",
     headerClassName: "table-header",
-    width: 400,
+    width: 200,
   },
   {
     field: "sender",
@@ -87,48 +88,30 @@ const columns = [
 ];
 
 const MessageManagement = () => {
-  const { loading, data, error } = useFetchData(
-    `${server}/api/v1/admin/messages`,
-    "dashboard-messages"
-  );
-
-  useErrors([
-    {
-      isError: error,
-      error: error,
-    },
-  ]);
-
   const [rows, setRows] = useState([]);
 
   useEffect(() => {
-    if (data) {
-      setRows(
-        data.messages.map((i) => ({
-          ...i,
-          id: i._id,
-          sender: {
-            name: i.sender.name,
-            avatar: transformImage(i.sender.avatar, 50),
-          },
-          createdAt: moment(i.createdAt).format("MMMM Do YYYY, h:mm:ss a"),
-        }))
-      );
-    }
-  }, [data]);
+    setRows(
+      dashboardData.messages.map((i) => ({
+        ...i,
+        id: i._id,
+        sender: {
+          name: i.sender.name,
+          avatar: transformImage(i.sender.avatar,50),
+        },
+        createdAt: moment(i.createdAt).format("MMMM Do YYYY, h:mm:ss a"),
+      }))
+    );
+  }, []);
 
   return (
     <AdminLayout>
-      {loading ? (
-        <Skeleton height={"100vh"} />
-      ) : (
-        <Table
+      <Table
           heading={"All Messages"}
           columns={columns}
           rows={rows}
           rowHeight={200}
         />
-      )}
     </AdminLayout>
   );
 };
