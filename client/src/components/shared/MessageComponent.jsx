c5ba84adfa9fd86efdf0df4ac842bf1434a51535
Vatsal bhalla <118548880+vatsalbhalla04@users.noreply.github.com>
2025-04-13 22:47:41 +0530

    Message Comp

--- client/src/components/shared/MessageComponent.jsx
+++ client/src/components/shared/MessageComponent.jsx
@@ -1,119 +1,65 @@
-/* eslint-disable react/jsx-key */
-import { useEffect, useState } from "react";
-import AdminLayout from "../../components/layout/AdminLayout";
-import Table from "../../components/shared/Table";
-import { dashboardData } from "../../constants/sampleData";
-import { fileFormat, transformImage } from "../../lib/features";
+import { Box, Typography } from "@mui/material";
+import React, { memo } from "react";
+import { lightBlue } from "../../constants/color";
 import moment from "moment";
-import { Avatar, Box, Stack } from "@mui/material";
-import RenderAttachment from "../../components/shared/RenderAttachment";
+import { fileFormat } from "../../lib/features";
+import RenderAttachment from "./RenderAttachment";
+import { motion } from "framer-motion";
 
-const columns = [
-  {
-    field: "id",
-    headerName: "ID",
-    headerClassName: "table-header",
-    width: 200,
-  },
-  {
-    field: "attachments",
-    headerName: "Attachments",
-    headerClassName: "table-header",
-    width: 200,
-    
-    renderCell: (params) => {
-      const { attachments } = params.row;
+const MessageComponent = ({ message, user }) => {
+  const { sender, content, attachments = [], createdAt } = message;
 
-      return attachments?.length > 0
-        ? attachments.map((i) => {
-            const url = i.url;
-            const file = fileFormat(url);
+  const sameSender = sender?._id === user?._id;
 
-            return (
-              <Box>
-                <a
-                  href={url}
-                  download
-                  target="_blank"
-                  style={{
-                    color: "black",
-                  }}
-                >
-                  {RenderAttachment(file, url)}
-                </a>
-              </Box>
-            );
-          })
-        : "No Attachments";
-
-    },
-  },
-
-  {
-    field: "content",
-    headerName: "Content",
-    headerClassName: "table-header",
-    width: 200,
-  },
-  {
-    field: "sender",
-    headerName: "Sent By",
-    headerClassName: "table-header",
-    width: 200,
-    renderCell: (params) => (
-      <Stack direction={"row"} spacing={"1rem"} alignItems={"center"}>
-        <Avatar alt={params.row.sender.name} src={params.row.sender.avatar} />
-        <span>{params.row.sender.name}</span>
-      </Stack>
-    ),
-  },
-  {
-    field: "chat",
-    headerName: "Chat",
-    headerClassName: "table-header",
-    width: 220,
-  },
-  {
-    field: "groupChat",
-    headerName: "Group Chat",
-    headerClassName: "table-header",
-    width: 100,
-  },
-  {
-    field: "createdAt",
-    headerName: "Time",
-    headerClassName: "table-header",
-    width: 250,
-  },
-];
-
-const MessageManagement = () => {
-  const [rows, setRows] = useState([]);
-
-  useEffect(() => {
-    setRows(
-      dashboardData.messages.map((i) => ({
-        ...i,
-        id: i._id,
-        sender: {
-          name: i.sender.name,
-          avatar: transformImage(i.sender.avatar,50),
-        },
-        createdAt: moment(i.createdAt).format("MMMM Do YYYY, h:mm:ss a"),
-      }))
-    );
-  }, []);
+  const timeAgo = moment(createdAt).fromNow();
 
   return (
-    <AdminLayout>
-      <Table
-          heading={"All Messages"}
-          columns={columns}
-          rows={rows}
-          rowHeight={200}
-        />
-    </AdminLayout>
+    <motion.div
+      initial={{ opacity: 0, x: "-100%" }}
+      whileInView={{ opacity: 1, x: 0 }}
+      style={{
+        alignSelf: sameSender ? "flex-end" : "flex-start",
+        backgroundColor: "white",
+        color: "black",
+        borderRadius: "5px",
+        padding: "0.5rem",
+        width: "fit-content",
+      }}
+    >
+      {!sameSender && (
+        <Typography color={lightBlue} fontWeight={"600"} variant="caption">
+          {sender.name}
+        </Typography>
+      )}
+
+      {content && <Typography>{content}</Typography>}
+
+      {attachments.length > 0 &&
+        attachments.map((attachment, index) => {
+          const url = attachment.url;
+          const file = fileFormat(url);
+
+          return (
+            <Box key={index}>
+              <a
+                href={url}
+                target="_blank"
+                download
+                style={{
+                  color: "black",
+                }}
+              >
+                {RenderAttachment(file, url)}
+              </a>
+            </Box>
+          );
+        })}
+
+      <Typography variant="caption" color={"text.secondary"}>
+        {timeAgo}
+      </Typography>
+    </motion.div>
   );
 };
 
-export default MessageManagement;
+export default memo(MessageComponent);
